f8ebd05a2c0814d3771ba443eb0cba75c120396c
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-07-18 21:32:27 +0200

    make i2pd log verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ mkdir -p /var/run/tor
 chown -R tor /var/run/tor
 tor -f /etc/tor/torrc &
 
-i2pd --conf=/etc/i2pd/i2pd.conf --tunconf=/etc/i2pd/tunnels.conf --daemon
+i2pd --conf=/etc/i2pd/i2pd.conf --tunconf=/etc/i2pd/tunnels.conf &
 
 privoxy --no-daemon --user privoxy /etc/privoxy/config
